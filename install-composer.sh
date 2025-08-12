b362d6ee17cdc323e3d1289734ce6f3866152d5a
AracelyC <125160548+aracely-campa@users.noreply.github.com>
2025-08-12 11:09:07 -0700

    commit

--- install-composer.sh
+++ install-composer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
+
+# Descargar Composer
 curl -sS https://getcomposer.org/installer | php
+
+# Instalar dependencias de Laravel
 php composer.phar install --no-dev --optimize-autoloader
