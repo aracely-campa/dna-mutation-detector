bfea04694315c32eb2cd44786438ede51fae1cec
AracelyC <125160548+aracely-campa@users.noreply.github.com>
2025-08-12 17:56:29 -0700

    Create star.sh

---
+++ star.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo " Esperando a que MongoDB Atlas esté listo..."
+until nc -z mycluster-shard-00-00.6tlbp.mongodb.net 27017; do
+    sleep 2
+done
+
+echo "MongoDB listo. Ejecutando migraciones..."
+php artisan migrate --force
+
+echo " Optimizando Laravel..."
+php artisan optimize
+php artisan storage:link
+
+echo "Iniciando servidor Laravel..."
+php artisan serve --host=0.0.0.0 --port=8000
